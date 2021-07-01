493766082cb8c382e544249f7abf2117e51d52eb
annis <nissinkentang@gmail.com>
2021-07-01 09:05:20 +0700

    activity 11

--- app/src/main/java/adapter/AdapterLihatTeman.java
+++ app/src/main/java/adapter/AdapterLihatTeman.java
@@ -1,22 +1,34 @@
 package adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.fbkelasa.MainActivity;
 import com.example.fbkelasa.R;
+import com.example.fbkelasa.TemanEdit;
 import com.example.fbkelasa.database.Teman;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
 public class AdapterLihatTeman  extends RecyclerView.Adapter<AdapterLihatTeman.ViewHolder>{
     private ArrayList<Teman> daftarTeman;
     private Context context;
+    private DatabaseReference databaseReference;
 
     public AdapterLihatTeman(ArrayList<Teman> daftarTeman, Context context) {
         this.daftarTeman = daftarTeman;
@@ -33,9 +45,64 @@ public class AdapterLihatTeman  extends RecyclerView.Adapter<AdapterLihatTeman.V
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        String nama = daftarTeman.get(position).getNama();
+        String kode, nama, telepon;
+        kode = daftarTeman.get(position).getKode();
+        nama = daftarTeman.get(position).getNama();
+        telepon = daftarTeman.get(position).getTelpon();
+
         holder.tvNama.setText(nama);
-    }
+
+        holder.tvNama.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu pm = new PopupMenu(v.getContext(), v);
+                pm.inflate(R.menu.popup2);
+
+                pm.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()){
+                            case R.id.edit:
+                                Bundle bundle = new Bundle();
+                                bundle.putString("kunci1", kode);
+                                bundle.putString("kunci2", nama);
+                                bundle.putString("kunci3", telepon);
+
+                                Intent intent = new Intent(v.getContext(), TemanEdit.class);
+                                intent.putExtras(bundle);
+                                v.getContext().startActivity(intent);
+
+                                break;
+                            case R.id.hapus:
+                                AlertDialog.Builder alertDlg = new AlertDialog.Builder(v.getContext());
+                                alertDlg.setTitle("Yakin" +nama+ "akan dihapus?");
+                                alertDlg.setMessage("Tekan Ya untuk menghapus").setCancelable(false)
+                                        .setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialog, int which) {
+                                                DeleteData(kode);
+                                                Toast.makeText(v.getContext(),"Data " +nama+ " berhasil dihapus",Toast.LENGTH_LONG).show();
+                                                Intent intent = new Intent(v.getContext(), MainActivity.class);
+                                                v.getContext().startActivity(intent);
+                                            }
+                                        }).setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.cancel();
+                                    }
+                                });
+                                AlertDialog adlg = alertDlg.create();
+                                adlg.show();
+                                break;
+                            }
+                            return true;
+                        }
+                    });
+                    pm.show();
+            }
+            });
+        holder.tvNama.setText(nama);
+        }
 
     @Override
     public int getItemCount() {
@@ -48,6 +115,11 @@ public class AdapterLihatTeman  extends RecyclerView.Adapter<AdapterLihatTeman.V
         ViewHolder(View v){
             super(v);
             tvNama = (TextView) v.findViewById(R.id.tv_nama);
+        }
+    }
+    public void DeleteData(String kode){
+        if (databaseReference !=null){
+            databaseReference.child("Teman").child(kode).removeValue();
         }
     }
 }
